848988272e52cf472107ff4bb4feda8e51e46102
Diego Barbas <barbasdiego@yahoo.com>
2023-12-20 21:10:20 -0300

    tercer desafio

--- src/app.js
+++ src/app.js
@@ -22,10 +22,11 @@ app.get("/products", async(req, res) => {
         let limit = parseInt(req.query.limit);
 
         if(limit) {
-            const arrayConLimite = arrayProductos.slice(o, limit);
+            const arrayConLimite = arrayProductos.slice(0, limit);
             return res.send(arrayConLimite);
         }
-
+        return res.send(arrayProductos);
+        
     } catch (error) {
         console.log(error);
         return res.send("Error al procesar la solicitud");
